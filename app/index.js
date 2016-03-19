f90399c0b7357ad0f7786dbe16e9687973923ed5
Kalen Dubrick <kalen3285@gmail.com>
2016-03-19 13:08:24 -0500

    Created HelloWorld component to test setup

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+function HelloWorld() {
+	return (
+		<div>Hello World!</div>
+	)
+}
+
+ReactDOM.render(<HelloWorld />, document.getElementById('app'))
